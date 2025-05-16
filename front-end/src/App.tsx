7318a0e2800aa8c821476e8a0da0d0c70581ed06
ranjit <ranjitdas2048@gmail.com>
2025-05-16 18:59:53 +0530

    added button

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,14 @@
 import './App.css'
 
 function App() {
-
-
+  const cliendId = String(Math.random());
+  console.log(cliendId)
   return (
     <>
       <div className='home'>
-        
+        <button className='button' onClick={()=>{
+          localStorage.setItem('clientId', cliendId)
+        }}>Register</button>
       </div>
     </>
   )
